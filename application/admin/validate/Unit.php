962ac30b278850f5e56a56041f93b0fd2864b1e3
bool <81001985@qq.com>
2019-01-28 17:20:20 +0800

    unit

---
+++ application/admin/validate/Unit.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\admin\validate;
+
+use think\Validate;
+
+class Unit extends Validate
+{
+    /**
+     * 定义验证规则
+     * 格式：'字段名'	=>	['规则1','规则2'...]
+     *
+     * @var array
+     */	
+	protected $rule = [
+        'name'      =>  'require|unique:unit',
+    ];
+    
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名'	=>	'错误信息'
+     *
+     * @var array
+     */	
+    protected $message = [
+        'name.require'      =>  '名称不能为空',
+        'name.unique'       =>  '名称已经存在',
+    ];
+}
