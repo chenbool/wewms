04c0ed798764ca293a9e8c9699a4d66fd13978b5
bool <81001985@qq.com>
2019-01-31 14:14:09 +0800

    *

---
+++ application/admin/service/Indepot.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\admin\service;
+use app\common\service\Base;
+
+/**
+ * 作者:bool
+ * QQ  :30024167
+ */
+class Indepot extends Base
+{
+	/**
+	 * [__construct 构造方法]
+	 */
+	function __construct()
+	{
+		parent::__construct();
+		// $this->model 	= new Model();
+	}
+
+}
